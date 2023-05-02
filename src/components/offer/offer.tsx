240347f7bd11028a3d866a26a1dd4b791b88aeef
mironovsergey <sergeymironov@protonmail.com>
2023-05-02 16:53:32 +0300

    Удалена форма со страницы предложения

--- src/components/offer/offer.tsx
+++ src/components/offer/offer.tsx
@@ -1,19 +1,71 @@
 import type { FC } from 'react';
+import type { TOffer } from '../../utils/types';
+import { useState, useEffect, useCallback } from 'react';
 import { useParams } from 'react-router-dom';
 import { useSelector } from '../../hooks/use-selector';
+import { useTelegram } from '../../hooks/use-telegram';
 import OfferDetails from '../offer-details/offer-details';
-import OfferForm from '../offer-form/offer-form';
+// import OfferForm from '../offer-form/offer-form';
 
 const Offer: FC = () => {
     const { id } = useParams();
     const { offers } = useSelector(({ offers }) => offers);
+    const [currentOffer, setCurrentOffer] = useState<TOffer | null>(null);
+    const telegramWebApp = useTelegram();
 
-    if (!id) {
-        return null;
-    }
+    const handleSendData = useCallback(
+        () => {
+            telegramWebApp.sendData(JSON.stringify({
+                offerId: currentOffer?.id
+            }));
+        },
+        [currentOffer?.id, telegramWebApp]
+    );
 
-    const offerId = parseInt(id, 10);
-    const currentOffer = offers.find((item) => item.id === offerId);
+    useEffect(
+        () => {
+            if (id) {
+                const offerId = parseInt(id, 10);
+                const offer = offers.find((item) => item.id === offerId);
+
+                if (offer) {
+                    setCurrentOffer(offer);
+                }
+            }
+        },
+        [id, offers]
+    );
+
+    useEffect(
+        () => {
+            telegramWebApp.onEvent('mainButtonClicked', handleSendData);
+
+            return () => {
+                telegramWebApp.offEvent('mainButtonClicked', handleSendData);
+            };
+        },
+        [handleSendData, telegramWebApp]
+    );
+
+    useEffect(
+        () => {
+            telegramWebApp.MainButton.setParams({
+                text: 'Записаться'
+            });
+        },
+        [telegramWebApp.MainButton]
+    );
+
+    useEffect(
+        () => {
+            if (!currentOffer) {
+                telegramWebApp.MainButton.hide();
+            } else {
+                telegramWebApp.MainButton.show();
+            }
+        },
+        [currentOffer, telegramWebApp.MainButton]
+    );
 
     if (!currentOffer) {
         return null;
@@ -23,7 +75,7 @@ const Offer: FC = () => {
         <div className="offer">
             <h1>Записаться на прием</h1>
             <OfferDetails offer={currentOffer} />
-            <OfferForm offerId={offerId} />
+            {/* <OfferForm offerId={offerId} /> */}
         </div>
     );
 };
